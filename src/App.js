439ac0974b4ce6850ee8b8ba006f5803d79c73a0
Himalaya Gupta <guptahimalaya2@gmail.com>
2021-07-29 01:47:44 +0530

    sab changa si

--- src/App.js
+++ src/App.js
@@ -1,22 +1,13 @@
 import React from "react";
 import Modal from "./Components/SearchModal/SearchModal";
-<<<<<<< HEAD
 import Login from "./Pages/Login/Login";
 import Signup from "./Pages/SIgnup/Signup";
-=======
 import Layout from './Pages/Layout'
->>>>>>> 222dc4724fdbcdb08b10f06b014361a8a0f53dc0
 
 function App() {
   return (
     <div className="App">
-<<<<<<< HEAD
-      {/* <Modal /> */}
-      <Login />
-      {/* <Signup /> */}
-=======
       <Layout/>
->>>>>>> 222dc4724fdbcdb08b10f06b014361a8a0f53dc0
     </div>
   );
 }
