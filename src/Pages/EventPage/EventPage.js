48494015c9bef41c145fda0b809034b322f15c8b
Himalaya Gupta <guptahimalaya2@gmail.com>
2021-08-03 23:57:46 +0530

    again commit

--- src/Pages/EventPage/EventPage.js
+++ src/Pages/EventPage/EventPage.js
@@ -2,12 +2,12 @@ import React from 'react'
 import EventInfo from '../../Components/EventInfo/EventInfo'
 import TaskCard from '../../Components/TaskCard/TaskCard'
 import './EventPage.css'
-
+import Chatbox from '../../Components/Chatbox/Chatbox'
 
 function EventPage() {
     return (
         <div className="eventPageContainer">
-            <EventInfo/>
+            {/* <EventInfo/>
             <div style={{ padding: "10px 15px", height:"100vh", overflow:"hidden" }}>
                 <TaskCard isComplete={false} bgColor="#353b48" />
                 <TaskCard isComplete={false} bgColor="#353b48" />
@@ -15,7 +15,8 @@ function EventPage() {
                 <TaskCard isComplete={false} bgColor="#353b48" />
                 <TaskCard isComplete={false} bgColor="#353b48" />
                 <TaskCard isComplete={false} bgColor="#353b48" />
-            </div>
+            </div> */}
+            <Chatbox/>
         </div>
     )
 }
